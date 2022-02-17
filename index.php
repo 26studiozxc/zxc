81bb3cee6ce9102673537db6cd6cb28a41de6c0f
26studiozxc <99875435+26studiozxc@users.noreply.github.com>
2022-02-17 13:33:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>26Studio</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,600;0,800;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <header class="header">
+        <div class="container">
+            <nav class="menu">
+                <ul class="menu__list">
+                    <li class="menu__logo"><a href="" class="menu__item"><img src="../icons/logo.svg" alt="logo" class="logo"></a></li>
+                </ul>
+                <ul class="menu__list">
+                    <li class="menu__link"><a href="" class="menu__item">Главная</a></li>
+                    <li class="menu__link"><a href="" class="menu__item">Группы</a></li>
+                    <li class="menu__link"><a href="" class="menu__item">Проекты</a></li>
+                    <li class="menu__link"><a href="" class="menu__item">Редактор кода</a></li>
+                </ul>
+                <ul class="menu__list">
+                    <li class="menu__link-right"><a href="auth.php" class="menu__item btn">Войти</a></li>
+                    <li class="menu__link-right"><a href="" class="menu__item btn">Регистрация</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div class="container">
+        <section class="main">
+        <img src="img/desktop1/waveReverce.png" alt="wave" class="wave">
+            <img src="img/desktop1/wave.png" alt="wave" class="wave_reverce">
+            <div class="title">
+                <div class="title__word">
+                    <h1 class="title__text">Добро пожаловать на студенческий сайт колледжа 26 КАДР</h1>
+                    <h2 class="subtitle">Здесь можно познакомиться с работами студентов, занимающихся ВЕБ-разработкой. А также просто с домашними заданиями по учебному плану</h2>
+                </div>
+                <div class="model__block">
+                        <img src="img/desktop1/Blocks.png" alt="3dmodel" class="model">
+                    </div>
+            </div>
+
+            <h2 class="contact__title">Контактные данные</h2>
+            <div class="contact">
+                <div class="contact__items">
+                    <div class="contact__text">Адрес: <br> <span class="textHidden">г. Москва, Севастопольский проспект 5а</span></div>
+                    <div class="contact__text">Станция метро: <br> <span class="textHidden">МЦК Крымская</span></div>
+                    <div class="contact__text">Директор <br> <span  class="textHidden">Быкова Елена Алексеевна</span></div>
+                </div>
+                <div class="contact__card"><iframe class="card" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4497.725486924383!2d37.60798443910045!3d55.69137403701993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54d17c90f5ed7%3A0x4543db10544818fe!2z0KHQtdCy0LDRgdGC0L7Qv9C-0LvRjNGB0LrQuNC5INC_0YAuLCA10JAsINCc0L7RgdC60LLQsCwgMTE3MTUy!5e0!3m2!1sru!2sru!4v1644254014731!5m2!1sru!2sru" width="600" height="330" style="border:0; border-radius: 3px;" allowfullscreen="" loading="lazy"></iframe></div>
+            </div>
+            <h2 class="contact__subtitle">Разработчики</h2>
+            <div class="developers">
+                <div class="developers__text">Хакназаров Игорь Дмитриевич</div>
+                <div class="developers__text">Сойма Владислав Евгеньевич</div>
+                <div class="developers__text">Катков Эльвин </div>
+            </div>
+        </section>
+    </div>
+
+    <footer class="footer">
+        <h2 class="footer__text">
+            <span class="footer__detail" > 26Studio LLC © 2022. All Rights Reserved.</span>
+            <span> Мы в социальных сетях:</span>
+         </h2>
+        <div class="social">
+            <a class="social__img" href=""></a>
+            <a class="social__img" href=""></a>
+            <!-- <a href=""><img class="social__img" src="icons/desktop1/vk.svg" alt="socialIcon"></a> -->
+            <!-- <a href=""><img class="social__img" src="icons/desktop1/inst.svg" alt="socialIcon"></a> -->
+        </div>
+    </footer>
+
+<script src="script.js"></script>
+</body>
+</html>
